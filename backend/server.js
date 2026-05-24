3ba496cf3b3ba4faa36ceef7f6eaf7b38c96fe5c
Kunal Chouhan <kunalchouhan037@gmail.com>
2026-05-24 17:15:11 +0530

    image working fix

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));
 
+app.use('/img', express.static(path.join(__dirname, '../img')));
 app.use(express.json());
 app.use(cors());
 
